a513b8c0566f0e67f745152a281dde88b75234c7
iweinberger <81303058+iweinberger@users.noreply.github.com>
2025-04-02 12:43:08 -0400

    Revert "Added nav bar"
    This reverts commit 571df3ce55eb7b141ffa4e0e4514200e807d054b.

--- app/donations/page.tsx
+++ app/donations/page.tsx
@@ -40,7 +40,6 @@ export default function Donations() {
           <a href="/#team">Team</a>
           <a href="/#projects">Projects</a>
           <a href="/#contact">Contact</a>
-          <a href="/donations">Donate</a>
         </div>
       </nav>
 
