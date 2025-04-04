90e716e1b93239058ef6f1a7c4cdb2590d7c694b
iweinberger <81303058+iweinberger@users.noreply.github.com>
2025-04-04 10:45:49 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -64,6 +64,10 @@ export default function Home() {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const forward_to_sponsors = () => {
+    document.location.href = "/sponsors"
+  };
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setFormStatus('loading');
@@ -232,7 +236,7 @@ export default function Home() {
               <h3>40</h3>
               <p>Team Members</p>
             </div>
-            <div className="stat" onclick="">
+            <div className="stat" onclick={() => forward_to_sponsors()}>
               <i className="fas fa-sack-dollar"></i>
               <h3>1</h3>
               <p>Sponsors</p>
