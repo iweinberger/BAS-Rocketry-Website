fd55cd935c5e21ca53a96048aff8b6018f3200c9
iweinberger <81303058+iweinberger@users.noreply.github.com>
2025-05-01 15:18:59 -0400

    Update danation page

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -181,17 +181,7 @@ export default function Home() {
       <main>
         <section>
         
-          <iframe
-            id="JotFormIFrame-251188477753267"
-            title="Donation Form"
-            onload="window.parent.scrollTo(0,0)"
-            allowtransparency="true"
-            allow="geolocation; microphone; camera; fullscreen"
-            src="https://form.jotform.com/251188477753267"
-            frameborder="0"
-            style="min-width:100%;max-width:100%;height:539px;border:none;"
-            scrolling="no"
-          >
+          <iframe id="JotFormIFrame-251188477753267" title="Donation Form" onload="window.parent.scrollTo(0,0)" allowtransparency="true" allow="geolocation; microphone; camera; fullscreen" src="https://form.jotform.com/251188477753267" frameborder="0" style="min-width:100%;max-width:100%;height:539px;border:none;" scrolling="no">
           </iframe>
           <script src='https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js'></script>
           <script>window.jotformEmbedHandler("iframe[id='JotFormIFrame-251188477753267']", "https://form.jotform.com/")</script>
