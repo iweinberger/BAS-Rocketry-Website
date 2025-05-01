1365ed99b94746a42019d8b26959e851023613d6
iweinberger <81303058+iweinberger@users.noreply.github.com>
2025-05-01 15:22:09 -0400

    Removal test of jotform

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -179,14 +179,7 @@ export default function Home() {
         <title>BAS Rocketry - Our Team</title>
       </head>
       <main>
-        <section>
-        
-          <iframe id="JotFormIFrame-251188477753267" title="Donation Form" onload="window.parent.scrollTo(0,0)" allowtransparency="true" allow="geolocation; microphone; camera; fullscreen" src="https://form.jotform.com/251188477753267" frameborder="0" style="min-width:100%;max-width:100%;height:539px;border:none;" scrolling="no">
-          </iframe>
-          <script src='https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js'></script>
-          <script>window.jotformEmbedHandler("iframe[id='JotFormIFrame-251188477753267']", "https://form.jotform.com/")</script>
-    
-        </section>
+
 
         <footer>
           <div className="footer-content">
