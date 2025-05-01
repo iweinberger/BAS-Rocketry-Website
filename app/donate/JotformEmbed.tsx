79d4853afd2e6cb4549eea42359fed2e99eba7e4
iweinberger <81303058+iweinberger@users.noreply.github.com>
2025-05-01 15:42:24 -0400

    Update from url

--- app/donate/JotformEmbed.tsx
+++ app/donate/JotformEmbed.tsx
@@ -12,7 +12,7 @@ export default function JotformEmbed() {
       id="JotFormIFrame-251188477753267"
       title="Donation Form"
       allow="geolocation; microphone; camera"
-      src="https://form.jotform.com/240996741314158"
+      src="https://form.jotform.com/251188477753267"
       frameBorder="0"
       scrolling="no"
       style={{
