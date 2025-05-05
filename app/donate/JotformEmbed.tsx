37454669b8f9b0e05b58354a87ad7113c39336a1
iweinberger <81303058+iweinberger@users.noreply.github.com>
2025-05-04 20:40:01 -0400

    Update JotformEmbed.tsx

--- app/donate/JotformEmbed.tsx
+++ app/donate/JotformEmbed.tsx
@@ -13,7 +13,6 @@
        title="Donation Form"
        allow="geolocation; microphone; camera"
        src="https://form.jotform.com/251188477753267"
-       src="https://bas-rocketry.pages.dev/forms"
        frameBorder="0"
        scrolling="no"
        style={{
