346bf698c38e9b017e0664e8d9b36bc111bcd1fd
iweinberger <81303058+iweinberger@users.noreply.github.com>
2025-05-05 18:55:32 -0400

    Fix mistake

--- app/page.tsx
+++ app/page.tsx
@@ -158,7 +158,7 @@ export default function Home() {
             <h3>Executive Director</h3>
             <div className="member-image ilan">
               <Image
-                src="/team/Ilan.png"
+                src="/Ilan.png"
                 alt="Ilan Weinberger"
                 width={200}
                 height={200}
