cd1964a4ce137756ff61a95a33ac48ba13741cf3
iweinberger <81303058+iweinberger@users.noreply.github.com>
2025-05-04 20:38:38 -0400

    Change donation form

--- app/donate/JotformEmbed.tsx
+++ app/donate/JotformEmbed.tsx
@@ -1,1 +1,29 @@
+'use client';
+ 
+ import { useEffect } from 'react';
+ 
+ export default function JotformEmbed() {
+   useEffect(() => {
+     // Optional: Log or trigger something once iframe loads
+   }, []);
+ 
+   return (
+     <iframe
+       id="JotFormIFrame-251188477753267"
+       title="Donation Form"
+       allow="geolocation; microphone; camera"
+       src="https://form.jotform.com/251188477753267"
+       src="https://bas-rocketry.pages.dev/forms"
+       frameBorder="0"
+       scrolling="no"
+       style={{
+         width: '1px',
+         minWidth: '100%',
+         height: '1500px',
+         height: '1200px',
+         border: 'none',
+       }}
+     />
+   );
+ }
 
